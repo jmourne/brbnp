662dca5e899fcf7038da18c64da58a2a1aeb4cd6
J. Mourne <critpathdigital@gmail.com>
2026-04-27 14:01:03 -0500

    Update tailwind.config.ts

--- src/styles/tailwind.config.ts
+++ src/styles/tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  // Add this 'content' section so Tailwind sees your files!
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../index.html",
+    "../../src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
