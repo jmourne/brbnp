59baff6b6e360147ff70c45728544582eb70f027
J. Mourne <critpathdigital@gmail.com>
2026-04-27 11:56:49 -0500

    Create tailwind.config.ts

---
+++ src/styles/tailwind.config.ts
@@ -0,0 +1,17 @@
+// default style
+export default {
+  theme: {
+    extend: {
+      colors: {
+        barbarian: {
+          red: '#e31b23',
+          dark: '#09090b',
+          zinc: '#18181b',
+        }
+      },
+      fontFamily: {
+        display: ['Oswald', 'sans-serif'], // For that aggressive header look
+      }
+    }
+  }
+}
